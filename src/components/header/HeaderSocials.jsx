13ff2e2ebad6dcce9d50004abd99efef82283b91
Tafadzwa Ndingindi <tafadzwandingindi@outlook.com>
2023-04-20 10:43:56 +0200

    Commit 1.1

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import {AiOutlineLinkedin, BsLinkedin} from 'react-icons/bs'
+import {AiOutlineLinkedin, BsLinkedin, BsWhatsapp} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://linkedin.com/in/tafadzwa-ndingindi-15336617b"><BsLinkedin/></a>
+        <a href="https://linkedin.com/in/tafadzwa-ndingindi-15336617b" target='_blank'><BsLinkedin/></a>
+        <a href="https://wa.me/+27623085980" target='_blank'><BsWhatsapp/></a>
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=tafadzwandingindi@outlook.com" target='_blank'>@</a>
     </div>
   )
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiFillHome} from 'react-icons'
+import {AiOutlineHome} from 'react-icons/ai'
+import {CgProfile} from 'react-icons/cg'
+import {MdWorkOutline} from 'react-icons/md'
+import {TiContacts} from 'react-icons/ti'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href=""><AiOutlineHome/></a>
+      <a href=""><CgProfile/></a>
+      <a href=""><MdWorkOutline/></a> 
+      <a href=""><TiContacts/></a>
+
+    </nav>
   )
 }
 
