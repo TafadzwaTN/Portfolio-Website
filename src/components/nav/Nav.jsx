ab8e158669563edc85613834074ec8a28c2b61c6
Tafadzwa Ndingindi <tafadzwandingindi@outlook.com>
2023-04-20 15:46:31 +0200

    Nav component .jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import './nav.css'
-import {AiFillHome} from 'react-icons'
 import {AiOutlineHome} from 'react-icons/ai'
 import {CgProfile} from 'react-icons/cg'
 import {MdWorkOutline} from 'react-icons/md'
 import {TiContacts} from 'react-icons/ti'
+import {BsTools} from 'react-icons/bs'
 
 const Nav = () => {
   return (
     <nav>
-      <a href=""><AiOutlineHome/></a>
-      <a href=""><CgProfile/></a>
-      <a href=""><MdWorkOutline/></a> 
-      <a href=""><TiContacts/></a>
+      <a href="#home"><AiOutlineHome/></a>
+      <a href="#about"><CgProfile/></a>
+      <a href="#Experience"><MdWorkOutline/></a> 
+      <a href="#Contacts"><TiContacts/></a>
+      <a href="#Services"><BsTools/></a>
 
     </nav>
   )
